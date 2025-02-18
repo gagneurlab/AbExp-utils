16a18dcc2c8345a841f26d6b1491e1a5cb86979a
George Tsitsiridis <gtsitsiridis@gmail.com>
2025-02-18 16:50:16 +0100

    send logs to stdout

--- packages/kipoi_enformer/src/kipoi_enformer/logger.py
+++ packages/kipoi_enformer/src/kipoi_enformer/logger.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 __all__ = ['logger', 'setup_logger']
 
@@ -7,6 +8,6 @@ logger = logging.getLogger('kipoi_enformer')
 
 def setup_logger(level=logging.INFO):
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+                        datefmt='%Y-%m-%d %H:%M:%S', stream=sys.stdout)
     logger.setLevel(level)
     return logger
